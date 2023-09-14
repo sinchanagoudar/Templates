6df9151f720c6dee7f88d49fd9c5fbe698c0d502
Anuprakashmp <104611795+Anuprakashmp@users.noreply.github.com>
2023-09-14 13:36:10 +0530

    error fixed

--- app/desktop/src/view/history/BrowseController.js
+++ app/desktop/src/view/history/BrowseController.js
@@ -2,23 +2,23 @@ Ext.define('App.view.history.BrowseController', {
     extend: 'App.view.widgets.BrowseController',
     alias: 'controller.historybrowse',
 
-    control: {
-        '#': {
-            reset: 'refresh'
-        }
-    },
+    // control: {
+    //     '#': {
+    //         reset: 'refresh'
+    //     }
+    // },
 
-    refresh: function() {
-        var vm = this.getViewModel();
-        vm.getStore('offices').reload();
-        vm.getStore('organizations').reload();
-        vm.getStore('recipients').reload();
-        vm.getStore('types').reload();
-    },
+    // refresh: function() {
+    //     var vm = this.getViewModel();
+    //     vm.getStore('offices').reload();
+    //     vm.getStore('organizations').reload();
+    //     vm.getStore('recipients').reload();
+    //     vm.getStore('types').reload();
+    // },
 
-    onDeleteAction: function(list, data) {
-        var store = this.getViewModel().getStore('history');
-        store.remove(data.record);
-        store.save();
-    }
+    // onDeleteAction: function(list, data) {
+    //     var store = this.getViewModel().getStore('history');
+    //     store.remove(data.record);
+    //     store.save();
+    // }
 });
--- app/desktop/src/view/person/BrowseController.js
+++ app/desktop/src/view/person/BrowseController.js
@@ -2,19 +2,19 @@ Ext.define('App.view.person.BrowseController', {
     extend: 'App.view.widgets.BrowseController',
     alias: 'controller.personbrowse',
 
-    control: {
-        '#': {
-            reset: 'refresh'
-        }
-    },
+    // control: {
+    //     '#': {
+    //         reset: 'refresh'
+    //     }
+    // },
 
-    refresh: function() {
-        var vm = this.getViewModel();
-        vm.getStore('offices').reload();
-        vm.getStore('organizations').reload();
-    },
+    // refresh: function() {
+    //     var vm = this.getViewModel();
+    //     vm.getStore('offices').reload();
+    //     vm.getStore('organizations').reload();
+    // },
 
-    onCreate: function() {
-        this.redirectTo('person/create');
-    }
+    // onCreate: function() {
+    //     this.redirectTo('person/create');
+    // }
 });
